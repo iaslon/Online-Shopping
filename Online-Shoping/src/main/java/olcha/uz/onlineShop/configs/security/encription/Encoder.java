9d5df38bbceda92a4329857314ba2dbe845e51a4
Aslonbek005 <73424408+Aslonbek099@users.noreply.github.com>
2022-10-15 14:47:11 +0500

    Add files via upload

---
+++ Online-Shoping/src/main/java/olcha/uz/onlineShop/configs/security/encription/Encoder.java
@@ -0,0 +1,9 @@
+package olcha.uz.onlineShop.configs.security.encription;
+
+/**
+ * @author "Khazratov Aslonbek"
+ * @since 10/15/2022 14:28 (Saturday)
+ * Online-Shoping/IntelliJ IDEA
+ */
+public class Encoder {
+}
---
+++ Online-Shoping/src/main/java/olcha/uz/onlineShop/controller/ApplicationConfigurer.java
@@ -0,0 +1,13 @@
+package olcha.uz.onlineShop.controller;
+
+/**
+ * @author "Khazratov Aslonbek"
+ * @since 10/15/2022 14:38 (Saturday)
+ * Online-Shoping/IntelliJ IDEA
+ */
+
+
+
+
+public class ApplicationConfigurer {
+}
